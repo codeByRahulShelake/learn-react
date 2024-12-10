06625d58356c4c9fa8d88f2dd394e681689fc045
Rahul Shelake <rahulshelake.connect@gmail.com>
2024-12-10 10:45:35 +0530

    learned about how to send object and array as a prop

--- 04-props-tailwind/src/App.jsx
+++ 04-props-tailwind/src/App.jsx
@@ -2,13 +2,19 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-
+let myArr = [1,2,3]
+let myObj = {
+  name : 'Rahul', 
+  age : 22
+}
   return (
     <>
      <h1 className="text-4xl font-bold  bg-gray-200 text-blue-600 p-4 rounded-lg mb-5">Used Tailwind</h1>
      <div className='cards'>
         <Card userName = 'Rahul'/>
         <Card userName = 'Sonali'/>
+        {/*<Card someArr = {myArr}  */}
+        {/*<Card someObj = {myObj}  */}
         <Card/>
      </div>
      
